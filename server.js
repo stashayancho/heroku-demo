e751dfdcc713c4b45d578adb552a49f7b041f301
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2018-08-29 10:55:49 -0500

    Use port env var

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const { layout, instructorUL } = require('./templates')
 const instructors = require('./instructors.json')
+const PORT = process.env.PORT || 3000
 
 app.get('/', async (req, res, next) => {
   res.send(layout(instructorUL(instructors)))
 })
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
   if (err) throw err;
   console.log('listening on port 3000')
 })
